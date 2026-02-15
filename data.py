e0d72471501e7ff9f6614cdcabebc2b274d5c003
sarosi <sarosigabor@gmail.com>
2026-02-16 00:27:10 +0100

    Add some fish to the database

--- data.py
+++ data.py
@@ -6,10 +6,11 @@ SPECIES_DATA = {
         "pH": (6.5, 8.0),
         "Hardness (dGH)": (4, 12),
     },
-    "Gourami": {
-        "Temperature (°C)": (22, 28),
+    "Honey Gourami": {
+        "Temperature (°C)": (20, 30),
         "pH": (6.0, 7.5),
         "Hardness (dGH)": (5, 15),
+        "Min Tank Size (L)": 40,
     },
     "Palacsinta algázó": {
         "Temperature (°C)": (18, 24),
@@ -20,18 +21,36 @@ SPECIES_DATA = {
         "Temperature (°C)": (20, 28),
         "pH": (5.5, 7.5),
         "Hardness (dGH)": (3, 15),
+        "Min Tank Size (L)": 80,
     },
     "Pfeffersalmler": {
         "Temperature (°C)": (20, 28),
         "pH": (5.0, 7.0),
         "Hardness (dGH)": (2, 10),
     },
+    "Rotkopfsalmler": {
+        "Temperature (°C)": (23, 30),
+        "pH": (5.5, 7.5),
+        "Hardness (dGH)": (2, 15),
+        "Min Tank Size (L)": 160,
+        "Min Tank lenght (cm)": 100,
+        "Min groupsize": 10
+    },
     "Neon Tetra": {
         "Temperature (°C)": (21, 26),
         "pH": (5.0, 7.0),
         "Hardness (dGH)": (2, 12),
+        "Min Tank Size (L)": 40,
+        "Min groupsize": 12
     },
-    "Amano": {
+    "Pencil tetra": {
+        "Temperature (°C)": (22, 27),
+        "pH": (6.0, 7.0),
+        "Hardness (dGH)": (2, 12),
+        "Min Tank Size (L)": 60,
+        "Min Tank lenght (cm)": 60,
+    },
+    "Amano shrimp": {
         "Temperature (°C)": (20, 25),
         "pH": (6.0, 7.5),
         "Hardness (dGH)": (1, 12),
@@ -51,4 +70,41 @@ SPECIES_DATA = {
         "pH": (6.5, 8.0),
         "Hardness (dGH)": (5, 20),
     },
+     "Apistogramma Tefe": {
+        "Temperature (°C)": (22, 28),
+        "pH": (5.5, 7.0),
+        "Hardness (dGH)": (2, 12),
+        "Min Tank Size (L)": 100,
+        "Min Tank lenght (cm)": 75,
+    },
+    "Danio margaritatus (Microrasboras galaxy)": {
+        "Other names": "Perlhuhnbärbling",
+        "Temperature (°C)": (18, 25),
+        "pH": (6.5, 7.5),
+        "Hardness (dGH)": (5, 20),
+        "Min Tank Size (L)": 60,
+        "Min Tank lenght (cm)": 75,
+        "Min groupsize": 12
+    },
+    "Apistogramma cacatuoides": {
+        "Other names": "Kakadu-Zwergbuntbarsch",
+        "Temperature (°C)": (23, 29),
+        "pH": (5.5, 7.5),
+        "Hardness (dGH)": (8, 12),
+        "Carbon hardness (dKH)": (3, 8),
+        "Min Tank Size (L)": 150,
+        "Min groupsize": 2
+    },
+     "Scalar Pterophyllum scalare Santa Isabel": {
+        "Temperature (°C)": (24, 30),
+        "pH": (6.0, 7.5),
+        "Hardness (dGH)": (2, 10),
+        "Min Tank Size (L)": 200,
+        "Min Tank lenght (cm)": 120,
+        "Min Tank height (cm)": 55
+    },
+    
+
+
+
 }
